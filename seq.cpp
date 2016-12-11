8530234ebb1872cfe3dd215902ad7ebfb7167103
Adithya Parthasarathy <adithya91@gmail.com>
2016-12-11 08:46:17 -0500

    Added CPU parallel version, refactored CUDA version

--- seq.cpp
+++ seq.cpp
@@ -1,58 +1,50 @@
-#include <cuda.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <cuda_runtime.h>
-#include <string.h>
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+#include <cstring>
+#include <thread>
+#include <vector>
 
 /* To index element (i,j) of a 2D array stored as 1D */
 #define index(i, j, N)  ((i)*(N)) + (j)
 
-//read a csv of input data X, a matrix and store it
-//implement function to calculate the gradient of of w wrt one datapoint
-//implement update step
-//implement kernel
+#define SAMPLES 50
+#define DIMENSIONALITY 10
+#define THREADS 5
+#define ITERATIONS 50
 
 int loadCSV(char *fileName, float *matrix, int rows, int cols);
 
-__global__
-void sgd_kernel(float* X, float* y, float *w,
+void sgd_kernel(float* X, float * y, float *w,
                 unsigned long long int num_samples,
                 unsigned long long int dimensionality,
                 unsigned long long int H,
-                int num_iterations);
+                int num_iterations,
+                int i, int j, int col_size);
 
-__device__
 void take_gradient_step(float *X, float *w, float *y,
                         unsigned long long int threadId,
                         unsigned long long int t,
                         unsigned long long int dimensionality,
                         unsigned long long int num_samples );
 
+using namespace std;
 
 int main()
 {
-    unsigned long long int num_samples = 5000;
-    unsigned long long int dimensionality = 20;
-    int num_threads = 5;
-    int num_iterations = 50;
+    unsigned long long int num_samples = SAMPLES;
+    unsigned long long int dimensionality = DIMENSIONALITY;
+    int num_threads = THREADS;
+    int num_iterations = ITERATIONS;
 
     unsigned long long int rows, cols;
     rows = num_samples;
     cols = dimensionality;
 
     //allocate the array
-    float *mat = (float*)malloc(rows * cols * sizeof(float));
-    float *maty = (float*)malloc(rows * sizeof(float));
-    
-    int result = loadCSV("./xmatrix.csv", mat, rows, cols);
-    int result2 = loadCSV("./yvector.csv", maty, 1, num_samples);
-
-    if (result < 0 || result2 < 0)
-    {
-        printf("Unable to load file\n");
-        return result;
-    }
+    // float *mat = (float*)malloc(rows * cols * sizeof(float));
+    // float *maty = (float*)malloc(rows * sizeof(float));
 
     int nThread = num_threads;
     unsigned long long int H = num_samples / nThread; //This is the number of samples each thread is going to handle
@@ -64,57 +56,18 @@ int main()
     float* X;
     float* w;
     float* y;
-    float *host_w = (float*)malloc(nThread * dimensionality * sizeof(float));
 
-    for (unsigned long long int i = 0; i < nThread; i++) {
-        for (unsigned long long int j = 0; j < dimensionality; j++) {
-            host_w[index(i, j, dimensionality)] = 0;
-        }
-    }
+    X = (float*) calloc(rows * cols, sizeof(float));
+    w = (float*) calloc(nThread * dimensionality, sizeof(float));
+    y = (float*) calloc(rows, sizeof(float));
 
-    cudaError_t cudaStatus;
-    cudaStatus = cudaMalloc(&X, rows * cols * sizeof(float));
-    if (cudaStatus != cudaSuccess)
+    int result = loadCSV((char*)"./xmatrix.csv", X, rows, cols);
+    int result2 = loadCSV((char*)"./yvector.csv", y, 1, num_samples);
+
+    if (result < 0 || result2 < 0)
     {
-        fprintf(stderr, "cudaMalloc of X failed!");
-        return -1;
-    }
-
-    cudaStatus = cudaMalloc(&w, nThread * dimensionality * sizeof(float));
-    if (cudaStatus != cudaSuccess)
-    {
-        fprintf(stderr, "cudaMalloc of w failed!");
-        return -1;
-    }
-
-    cudaStatus = cudaMalloc(&y, rows * sizeof(float));
-    if (cudaStatus != cudaSuccess)
-    {
-        fprintf(stderr, "cudaMalloc of y failed!");
-        return -1;
-    }
-
-    cudaStatus = cudaMemcpy(X, mat, rows * cols * sizeof(float), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess)
-    {
-        fprintf(stderr, "cudaMemcpy of X to device failed!");
-        return -1;
-    }
-
-    cudaStatus = cudaMemcpy(
-        w, host_w, nThread * dimensionality * sizeof(float), cudaMemcpyHostToDevice);
-
-    if (cudaStatus != cudaSuccess)
-    {
-        fprintf(stderr, "cudaMemcpy of w to device failed!");
-        return -1;
-    }
-
-    cudaStatus = cudaMemcpy(y, maty, rows * sizeof(float), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess)
-    {
-        fprintf(stderr, "cudaMemcpy of w to device failed!");
-        return -1;
+        printf("Unable to load file\n");
+        return result;
     }
 
     int blockWidth = 128;
@@ -123,15 +76,24 @@ int main()
         totalBlocks++;
 
     printf("totalBLock is %d, blockWidth is %d\n", totalBlocks, blockWidth);
-    sgd_kernel << <totalBlocks, blockWidth >> > (X, y, w, rows, cols, H, num_iterations);
 
-    cudaStatus = cudaMemcpy(
-        host_w, w, nThread * dimensionality * sizeof(float), cudaMemcpyDeviceToHost);
+    vector<thread*> pthreads;
 
-    if (cudaStatus != cudaSuccess)
-    {
-        fprintf(stderr, "cudaMemcpy of w to host failed!");
-        return -1;
+    for (int i = 0; i < totalBlocks; i++) {
+
+        for (int j = 0; j < blockWidth; j++) {
+
+            thread* pthread = new thread(
+                sgd_kernel, 
+                X, y, w, rows, cols, H, num_iterations, i, j, blockWidth);
+
+            pthreads.push_back(pthread);
+        }
+    }
+
+    // Wait for all to complete
+    for (int i = 0; i < THREADS; i++) {
+        pthreads[i]->join();
     }
 
     printf("printing gpu result\n");
@@ -140,7 +102,7 @@ int main()
     {
         for (int i = 0; i < nThread; i++)
         {
-            printf("% f", host_w[index(i, j, dimensionality)]);
+            printf("% f", w[index(i, j, dimensionality)]);
         }
         printf("\n");
     }
@@ -154,7 +116,7 @@ int main()
         float sum = 0;
         for (int i = 0; i < nThread; i++)
         {
-            sum += host_w[index(i, j, dimensionality)];
+            sum += w[index(i, j, dimensionality)];
         }
         final_w[j] = sum / nThread;
     }
@@ -166,25 +128,29 @@ int main()
     }
     printf("\n");
 
-    free(mat);
-    free(maty);
-    free(host_w);
+    // free(mat);
+    // free(maty);
+    free(X);
+    free(y);
+    free(w);
     free(final_w);
 
     return 0 ;
 
 }
 
-__global__
 void sgd_kernel(float* X, float * y, float *w,
                 unsigned long long int num_samples,
                 unsigned long long int dimensionality,
                 unsigned long long int H,
-                int num_iterations)
+                int num_iterations,
+                int i, int j, int col_size)
 {
-    unsigned long long int s = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned long long int s = i * col_size + j;
     unsigned long long int startIndex = s * H;
     unsigned long long int endIndex = startIndex + H;
+
+    // printf("In %d, %d = %d\n", i, j, s);
 
     /*if (s < 5){
         printf("%llu, %llu, %llu, %llu\n", num_samples, dimensionality, H, num_iterations);
@@ -213,7 +179,6 @@ void sgd_kernel(float* X, float * y, float *w,
     }
 }
 
-__device__
 void take_gradient_step(float *X, float *w, float *y, 
                         unsigned long long int threadId,
                         unsigned long long int t,
@@ -224,14 +189,14 @@ void take_gradient_step(float *X, float *w, float *y,
     float stepSize = 0.05;
     for (unsigned long long int j = 0; j < dimensionality; j++)
     {
-        // printf("thread id %llu, f %f, s %f\n", 
-        //     threadId, X[index(t, j, dimensionality)], w[index(threadId, j, dimensionality)]);
+        printf("thread id %llu, f %f, s %f\n", 
+            threadId, X[index(t, j, dimensionality)], w[index(threadId, j, dimensionality)]);
 
         xwsum += X[index(t, j, dimensionality)] * w[index(threadId, j, dimensionality)];
     }
     xwsum -= y[t];
     
-    //printf("thread id %llu, t %llu, xwsum %f\n", threadId, t, xwsum);
+    // printf("thread id %llu, t %llu, xwsum %f\n", threadId, t, xwsum);
 
     for (unsigned long long int j = 0; j < dimensionality; j++)
     {
